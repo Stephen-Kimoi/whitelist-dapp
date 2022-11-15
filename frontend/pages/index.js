70fa38263c03df0ea3267202cf723dfa4609008b
Steve Kimoi <moenisteve@gmail.com>
2022-11-15 16:25:29 +0300

    changing the contract address

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,9 +4,8 @@ import Web3Modal from "web3modal";
 import { providers, Contract } from "ethers" 
 import { useEffect, useRef, useState } from "react"
 import { abi } from '../utils/Whitelist.json'
-// import { WHITELIST_CONTRACT_ADDRESS, abi } from '../constants'
+import { WHITELIST_CONTRACT_ADDRESS } from '../constants'
 
-const WHITELIST_CONTRACT_ADDRESS = "0x4B30378F265A7d42c7596558fFd4A3fDF2e51ceD"
 
 export default function Home() {
   const [walletConnected, setWalletConnected] = useState(false); 
